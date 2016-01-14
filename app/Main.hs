fe453a92558f8aa983477480b62167b895b09385
masatoko <masatokodo@gmail.com>
2016-01-14 20:30:13 +0900

    Remove dot square

--- app/Main.hs
+++ app/Main.hs
@@ -12,15 +12,6 @@ main = runProtonic render
 render :: ProtoT ()
 render = do
   t <- P.frame
+  let size = abs $ sin $ fromIntegral t / (60 :: Double)
   P.clearBy $ V4 0 0 0 255
-  mapM_ (dot t) [V2 (x*16) (y*16) | x <- [0..40], y <- [0..30]]
-  where
-    dot :: Integer -> V2 Int -> ProtoT ()
-    dot t pos@(V2 x y) = P.testText pos (V4 r g b 255) "."
-      where
-        x' = x
-        y' = y + 128
-        t' = fromIntegral t
-        r = fromIntegral $ (x' * y' + t') `mod` 256
-        g = fromIntegral $ x' `mod` 256
-        b = fromIntegral $ y' `mod` 256
+  P.testText (V2 100 100) (V4 255 255 255 255) $ show (size :: Double)

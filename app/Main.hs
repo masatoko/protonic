ea9adb5977f22731f325709d34386928633f08c9
masatoko <masatokodo@gmail.com>
2016-01-14 10:48:04 +0900

    create Renderer

--- app/Main.hs
+++ app/Main.hs
@@ -3,5 +3,4 @@ module Main where
 import Protonic
 
 main :: IO ()
-main = runProto $
-  putStrLn "protonic"
+main = runProto
--- src/Protonic.hs
+++ src/Protonic.hs
@@ -1,12 +1,43 @@
 module Protonic where
 
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Exception (bracket_)
+import           Control.Concurrent  (threadDelay)
+import           Control.Exception   (bracket, bracket_)
+import qualified Data.Text           as T
+import           Linear.V4
+import           System.IO
 
-import qualified Graphics.UI.SDL.TTF   as TTF
+import qualified Graphics.UI.SDL.TTF as TTF
+import           SDL                 (($=))
 import qualified SDL
 
-runProto :: IO a -> IO a
-runProto work =
-  bracket_ SDL.initializeAll SDL.quit $
-    TTF.withInit work
+runProto :: IO ()
+runProto = withSDL $ TTF.withInit $ withRenderer loop
+  where
+    withSDL = bracket_ SDL.initializeAll SDL.quit
+    --
+    loop r = do
+      threadDelay 1000000
+      putChar '.' >> hFlush stdout
+      --
+      render r
+      --
+      loop r
+
+withRenderer :: (SDL.Renderer -> IO a) -> IO a
+withRenderer work = withW $ withR work
+  where
+    withW f = bracket (SDL.createWindow (T.pack "protonic") SDL.defaultWindow)
+                      SDL.destroyWindow
+                      f
+    withR f win = bracket (SDL.createRenderer win (-1) SDL.defaultRenderer)
+                          SDL.destroyRenderer
+                          f
+
+render :: SDL.Renderer -> IO ()
+render r = do
+  SDL.rendererDrawColor r $= V4 255 255 0 255
+  SDL.clear r
+  --
+  -- Rendering
+  --
+  SDL.present r

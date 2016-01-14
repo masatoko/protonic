c55e2b5b54ad4030c0d091c2ad15f1fd2740498c
masatoko <masatokodo@gmail.com>
2016-01-14 19:29:21 +0900

    Test rendering dot

--- app/Main.hs
+++ app/Main.hs
@@ -12,4 +12,13 @@ main = runProtonic render
 render :: ProtoT ()
 render = do
   P.clearBy $ V4 0 0 0 255
-  P.testText (V2 100 (100::Int)) (V4 255 255 255 255) "protonic"
+  mapM_ dot [V2 (x*16) (y*16) | x <- [0..40], y <- [0..30]]
+  where
+    dot :: V2 Int -> ProtoT ()
+    dot pos@(V2 x y) = P.testText pos (V4 r g b 255) "."
+      where
+        x' = x
+        y' = y + 128
+        r = fromIntegral $ (x' * y') `mod` 256
+        g = fromIntegral $ x' `mod` 256
+        b = fromIntegral $ y' `mod` 256

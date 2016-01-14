2e5417d90cbc0e197cc79da7e34c2f70bbfd9e1a
masatoko <masatokodo@gmail.com>
2016-01-14 23:39:49 +0900

    Test rotating Sprite

--- app/Main.hs
+++ app/Main.hs
@@ -22,7 +22,18 @@ main =
 render :: App -> ProtoT ()
 render (App sprite) = do
   t <- P.frame
-  let size = abs $ sin $ fromIntegral t / (60 :: Double)
+  let deg = (*360) $ abs $ sin $ fromIntegral t / (60 :: Double)
   P.clearBy $ V4 0 0 0 255
-  P.testText (V2 100 100) (V4 255 255 255 255) $ show (size :: Double)
-  P.renderS sprite (V2 100 200) (Just 90)
+  P.testText (V2 100 100) (V4 255 255 255 255) $ show (deg :: Double)
+  P.renderS sprite (V2 100 200) (Just deg)
+  mapM_ (work . (\i -> deg + (i * 60))) [0..6]
+  where
+    work :: Double -> ProtoT ()
+    work deg =
+      P.renderS sprite (V2 x y) (Just deg)
+      where
+        rad = pi * deg / 180
+        dx = cos rad * 50
+        dy = sin rad * 50
+        x = truncate $ 100 + dx
+        y = truncate $ 200 + dy

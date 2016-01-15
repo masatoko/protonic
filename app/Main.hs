3db40396eb1604fdf0896731edf429dc23d0a88e
masatoko <masatokodo@gmail.com>
2016-01-15 11:35:53 +0900

    Give chance of freeing app

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
 module Main where
 
+import Control.Exception (bracket)
+import           Control.Monad.Managed (managed, runManaged)
 import           Control.Monad.State
 import           Linear.V2
 import           Linear.V4
 
-import           Protonic            (ProtoT, runGame, runProtoT, withProtonic)
-import qualified Protonic            as P
+import           Protonic              (ProtoT, runGame, runProtoT,
+                                        withProtonic)
+import qualified Protonic              as P
 
 data App = App
   { appState  :: Double
@@ -14,14 +17,18 @@ data App = App
 
 main :: IO ()
 main =
-  withProtonic $ \proto -> do
-    (app,_) <- runProtoT proto initializeApp
-    runGame proto app update render
+  withProtonic $ \proto ->
+    runManaged $ do
+      app <- managed $ bracket (fst <$> runProtoT proto initializeApp) freeApp
+      liftIO $ runGame proto app update render
   where
     initializeApp :: ProtoT App
     initializeApp = do
       font <- P.newFont 30
       App 0 <$> P.newSprite font (V4 100 200 255 255) "@"
+    --
+    freeApp :: App -> IO ()
+    freeApp app = P.freeSprite $ appSprite app
 
 update :: App -> ProtoT App
 update app = snd <$> runStateT go app
@@ -35,11 +42,11 @@ update app = snd <$> runStateT go app
 render :: App -> ProtoT ()
 render (App stt sprite) = do
   P.clearBy $ V4 0 0 0 255
-  P.testText (V2 100 50) white $ show stt
+  P.testText center white $ (\i -> show (i :: Int)) $ truncate stt
   mapM_ (stamp stt) [0..6]
   where
     white = V4 255 255 255 255
-    center = V2 150 150
+    center = V2 100 150
     --
     stamp :: Double -> Int -> ProtoT ()
     stamp base i =

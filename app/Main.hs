d2345aa9633a38c5844158274c92676b77817f8d
masatoko <masatokodo@gmail.com>
2016-01-19 04:05:27 +0900

    Add transit function to Scene

--- app/Main.hs
+++ app/Main.hs
@@ -15,6 +15,8 @@ import           Protonic            (Metapad, ProtoT, Render, Scene (..),
                                       withProtonic)
 import qualified Protonic            as P
 
+data Title = Title
+
 data Game = Game
   { gSprite :: P.Sprite
   , gDeg    :: !Double
@@ -26,10 +28,13 @@ initGame = do
   font <- P.newFont 50
   char <- P.newSprite font (V4 255 255 255 255) "@"
   P.freeFont font
+  liftIO . putStrLn $ "init Game"
   return $ Game char 0 0
 
 freeApp :: MonadIO m => Game -> m ()
-freeApp = liftIO . P.freeSprite . gSprite
+freeApp g = liftIO $ do
+  P.freeSprite . gSprite $ g
+  putStrLn "free Game"
 
 resetApp :: Game -> Game
 resetApp (Game s _ _) = Game s 0 0
@@ -37,10 +42,8 @@ resetApp (Game s _ _) = Game s 0 0
 main :: IO ()
 main =
   withProtonic conf $ \proto -> do
-    _ <- runProtoT proto $ do
-      g <- initGame
-      runScene titleScene g
-      freeApp g
+    _ <- runProtoT proto $
+      runScene titleScene Title
     return ()
   where
     conf = P.defaultConfig {P.winSize = V2 300 300}
@@ -55,43 +58,35 @@ gamepad = flip execState newPad $ do
   modify . addAction $ P.hold SDL.ScancodeF Go
   modify . addAction $ P.pressed SDL.ScancodeReturn Enter
 
-titleScene :: Scene Game Action
-titleScene = Scene gamepad update render
+titleScene :: Scene Title Action
+titleScene = Scene gamepad update render transit
   where
-    update :: Update Game Action
-    update _ as g = return $
-      if Enter `elem` as
-        then (P.next mainScene g, resetApp g)
-        else (P.continue, g)
+    update :: Update Title Action
+    update _ _ = return
 
-    render :: Render Game
+    render :: Render Title
     render _ = P.printTest (V2 10 100) (V4 0 255 255 255) "Press Enter key to start"
 
+    transit as _ = if Enter `elem` as
+                     then P.next mainScene <$> initGame
+                     else return P.continue
+
 mainScene :: Scene Game Action
-mainScene = Scene gamepad update render
+mainScene = Scene gamepad update render transit
   where
     update :: Update Game Action
-    update stt as game = do
-      g' <- snd <$> runStateT go game
-      return (trans g', g')
+    update stt as = execStateT go
       where
-        go :: StateT Game IO ()
+        go :: StateT Game ProtoT ()
         go = do
           mapM_ count as
           setDeg
 
-        count :: Action -> StateT Game IO ()
+        count :: Action -> StateT Game ProtoT ()
         count Go = modify (\a -> let c = gCount a in a {gCount = c + 1})
         count _  = return ()
 
         setDeg = modify (\g -> g {gDeg = fromIntegral (frameCount stt `mod` 360)})
-
-        trans g = work (gCount g)
-          where
-            work cnt
-              | cnt > 30        = P.next (clearScene cnt) g
-              | Enter `elem` as = P.push pauseScene g
-              | otherwise       = P.continue
 
     render :: Render Game
     render (Game s d i) = do
@@ -102,23 +97,30 @@ mainScene = Scene gamepad update render
       let progress = replicate i '>' ++ replicate (30 - i) '-'
       P.printTest (V2 10 140) (V4 255 255 255 255) $ T.pack progress
 
+    transit as g = work (gCount g)
+      where
+        work cnt
+          | cnt > 30        = freeApp g >> return (P.next (clearScene cnt) g)
+          | Enter `elem` as = return $ P.push pauseScene g
+          | otherwise       = return P.continue
+
 pauseScene :: Scene Game Action
-pauseScene = Scene gamepad update render
+pauseScene = Scene gamepad update render transit
   where
-    update :: Update Game Action
-    update _ as g = return (if Enter `elem` as then P.end else P.continue, g)
+    update _ _ = return
     render :: Render Game
     render _ = do
       P.clearBy $ V4 50 50 0 255
       P.printTest (V2 10 100) (V4 255 255 255 255) "PAUSE"
+    transit as _ = return $ if Enter `elem` as then P.end else P.continue
 
 clearScene :: Int -> Scene Game Action
-clearScene score = Scene gamepad update render
+clearScene score = Scene gamepad update render transit
   where
-    update :: Update Game Action
-    update _ as g = return (if Enter `elem` as then P.next titleScene g else P.continue, g)
+    update _ _ = return
     render :: Render Game
     render _ = do
       P.clearBy $ V4 0 0 255 255
       P.printTest (V2 10 100) (V4 255 255 255 255) "CLEAR!"
       P.printTest (V2 10 120) (V4 255 255 255 255) $ T.pack ("Score: " ++ show score)
+    transit as _ = return $ if Enter `elem` as then P.next titleScene Title else P.continue
